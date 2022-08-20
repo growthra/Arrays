f050adbcf92aba1f967afaeee0d0dc25064c67e4
growthra <111641794+growthra@users.noreply.github.com>
2022-08-20 17:49:09 +0530

    Create TwoSum_BruteFoce_Index.js

---
+++ TwoSum_BruteFoce_Index.js
@@ -0,0 +1,20 @@
+
+// Two Sum Brute Force way
+
+const TwoSum= function GetTwoSum(nums,target){
+
+  for(let p1=0;p1<nums.length;p1++){
+    const numberToFind= target-nums[p1];
+    for(let p2=p1+1;p2<nums.length;p2++){
+      if(numberToFind==nums[p2])
+        return [p1,p2];
+    }
+  }
+}
+
+const nums=[1,3,7,9,2,10];
+
+const result=TwoSum(nums,19);
+
+
+console.log(result);
